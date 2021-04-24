87d8f823ec55d37d43e9550123abeb244760c5eb
wangedin <wangedin@hotmail.com>
2021-04-24 10:52:19 +0100

    20200424_02

--- emoji_story/__init__.py
+++ emoji_story/__init__.py
@@ -21,7 +21,7 @@ from emoji_story.utils import Emoji
 basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 
-def create_app(config_name='testing'):
+def create_app(config_name='production'):
     if config_name is None:
         config_name = os.getenv('FLASK_CONFIG', 'development')
 
