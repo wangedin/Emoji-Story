f66ebfa52f5ef9856d7f3eb7902fe7eee0541b79
wangedin <wangedin@hotmail.com>
2021-04-24 19:55:24 +0100

    20200424_06

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,3 @@
-import os
-from dotenv import load_dotenv
-
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
-
-
 from emoji_story import create_app
 
 app = create_app('production')
